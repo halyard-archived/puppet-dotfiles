0990021bfb3992eaac87d1a2267a4fdc72952354
Les Aker <me@lesaker.org>
2014-09-26 19:45:35 -0400

    don't use tmux

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,7 +24,7 @@ class dotdotdot(
   }
 
   exec { 'vundle install':
-    command => "${boxen::config::home}/homebrew/bin/tmux new-session -s vundle '${boxen::config::home}/homebrew/bin/vim -E -c BundleInstall -c qall'",
+    command => "${boxen::config::home}/homebrew/bin/vim -E -c BundleInstall -c qall",
     creates => "/Users/${::boxen_user}/.vim/bundle/DetectIndent/README",
     provider => "shell",
     require => [
